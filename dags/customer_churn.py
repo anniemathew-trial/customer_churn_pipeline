fd86ed7877e8d0bb97f45952f6d555afb88d6219
First Last <annie.mthw21@gmail.com>
2025-02-28 06:46:02 +0000

    Updating dag

--- dags/customer_churn.py
+++ dags/customer_churn.py
@@ -18,6 +18,7 @@ with DAG(
     pull_task = BashOperator(
         task_id="pull",
 	bash_command="cd /opt/airflow && dvc pull && git pull",
+	user = "airflow"
     )
     # Data ingestion and raw data storage to Amazon S3 using DVC
     data_ingestion_task = BashOperator(
