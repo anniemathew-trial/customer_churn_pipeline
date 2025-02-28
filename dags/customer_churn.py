222f5f7cbd489c3d5d83677789cabe6fe53e603e
anniemathew-trial <annie.mthw21@gmail.com>
2025-02-28 23:37:22 +0530

    Update customer_churn.py
    Signed-off-by: anniemathew-trial <annie.mthw21@gmail.com>

--- dags/customer_churn.py
+++ dags/customer_churn.py
@@ -85,7 +85,7 @@ with DAG(
         bash_command = """
 			cd /opt/airflow/customer_churn_stats/feature_repo && \
                         feast apply && \
-                        feast materialize-incremental $(date -u +"%Y-%m-%dT%H:%M:%S")
+                        feast materialize-incremental $(date -u +'%Y-%m-%dT%H:%M:%S') && \
                         git commit -m "Updated feature store" -a && \
                         git push
                     """
