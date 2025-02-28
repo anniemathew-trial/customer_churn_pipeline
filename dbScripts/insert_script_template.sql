ccb57d45ebf2c565f666548a6acfd70e5d01bac8
anniemathew-trial <annie.mthw21@gmail.com>
2025-02-28 15:40:27 +0530

    Add files via upload
    Signed-off-by: anniemathew-trial <annie.mthw21@gmail.com>

---
+++ dbScripts/insert_script_template.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers (CreditScore,	Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember,
+	EstimatedSalary, Exited, Geography_Germany,Geography_Spain,Gender_Male,  CreditScoreTenureRatio, 
+	TenureAgeRatio, BalanceSEstimatedalaryRatio, BalanceAgeRatio
+)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)
