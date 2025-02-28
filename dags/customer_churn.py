629e08bf9ac397a31e9bd58a3d4bcb2cf1188f10
First Last <annie.mthw21@gmail.com>
2025-02-28 03:11:01 +0000

    Initial Commit

---
+++ dags/customer_churn.py
@@ -0,0 +1,111 @@
+from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "Mathew Annie Thampy (2023DC04283)"
+}
+
+
+with DAG(
+    dag_id="customer_churn_ml_pipeline",
+    start_date=days_ago(1),
+    schedule_interval="0 8 * * *", # Daily at 8:00 AM UTC
+    catchup=False,
+) as dag:
+    # Data ingestion and raw data storage to Amazon S3 using DVC
+    data_ingestion_task = BashOperator(
+        task_id = 'data_ingestion_task',
+        bash_command = """python /opt/airflow/executables/data_ingestion.py && \
+                        dvc add /opt/airflow/data/raw && \
+                        git add /opt/airflow/data/raw.dvc && \
+                        git commit -m "Updated raw data version" && \
+                        dvc push
+                    """,
+    )
+    
+    data_validation_task = BashOperator(
+        task_id = 'data_validation_task',
+        bash_command = """
+                        dvc pull /opt/airflow/data/raw
+                        python /opt/airflow/executables/data_validation.py && \
+                        dvc add /opt/airflow/reports/csv_validation_report.csv && \
+                        git add /opt/airflow/reports/csv_validation_report.csv.dvc && \
+                        git commit -m "Updated validation" && \
+                        dvc push
+                    """
+    )
+
+    data_preparation_task = BashOperator(
+        task_id = 'data_preparation_task',
+        bash_command = """python /opt/airflow/executables/data_preparation.py && \
+                        dvc add /opt/airflow/data/cleaned && \
+                        dvc add /opt/airflow/visualization && \
+                        git add /opt/airflow/data/cleaned.dvc && \
+                        git add /opt/airflow/visualization.dvc && \
+                        git commit -m "Updated cleaned data version" && \
+                        dvc push
+                    """
+    )
+
+    data_transformation_task = BashOperator(
+        task_id = 'data_transformation_task',
+        bash_command = """
+                        dvc pull /opt/airflow/data/cleaned
+                        python /opt/airflow/executables/data_transformation.py && \
+                        dvc add /opt/airflow/data/transformed && \
+                        git add /opt/airflow/data/transformed.dvc && \
+                        git commit -m "Updated transformed data version" && \
+                        dvc push
+                    """
+    )
+
+    data_storage_task = BashOperator(
+        task_id = 'data_storage_task',
+        bash_command = """
+                        dvc pull /opt/airflow/data/transformed
+                        python /opt/airflow/executables/data_storage.py && \
+                        dvc add /opt/airflow/data/storage && \
+                        git add /opt/airflow/data/storage.dvc && \
+                        git commit -m "Updated stored data version" && \
+                        dvc push
+                    """
+    )
+
+    feature_store_task = BashOperator(
+        task_id = 'feature_store_task',
+        bash_command = """
+                        feast init
+                        python /opt/airflow/executables/feature_store.py && \
+                        dvc add /opt/airflow/data/features && \
+                        git add /opt/airflow/data/features.dvc && \
+                        git commit -m "Updated feature store" && \
+                        dvc push
+                    """
+    )
+
+    model_training_task = BashOperator(
+            task_id = 'model_training_task',
+            bash_command = """python /opt/airflow/executables/model_training.py && \
+                            dvc add /opt/airflow/models/model.pki && \
+                            git add /opt/airflow/models/model.pki.dvc && \
+                            git commit -m "Trained model version" && \
+                            dvc push
+                        """
+    )
+    model_evaluation_task = BashOperator(
+            task_id = 'model_evaluation_task',
+            bash_command = """
+                            dvc pull /opt/airflow/models/model.pkl 
+                            python /opt/airflow/executables/model_evaluation.py && \
+                            dvc add /opt/airflow/reports/evaluation.csv && \
+                            git add /opt/airflow/reports/evaluation.csv.dvc && \
+                            git commit -m "Updated model evaluation report" && \
+                            dvc push
+                        """
+    )
+
+data_ingestion_task >> data_validation_task >> data_preparation_task >> data_transformation_task >> data_storage_task >> feature_store_task
+feature_store_task >> model_training_task >> model_evaluation_task
---
+++ executables/data_ingestion.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+import pandas as pd
+import logging
+import pyodbc
+
+#create log file if it does not exist
+ingestion_log_file = "/opt/airflow/logs/data_ingestion.log"
+logging.root.handlers = []
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO , filename=ingestion_log_file)
+
+# set up logging
+console = logging.StreamHandler()
+console.setLevel(logging.ERROR)
+
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+console.setFormatter(formatter)
+logging.getLogger("").addHandler(console)
+
+def ingest_csv(filename):
+    try:
+        logging.info(f"Reading data from CSV file {filename}")
+        data = pd.read_csv(f'/opt/airflow/data/{filename}')
+        p = Path('/opt/airflow/data/raw')
+        p.mkdir(parents = True, exist_ok = True)
+        data.to_csv(f"/opt/airflow/data/raw/{filename}", index=False)
+        logging.info(f"Data from CSV {filename} ingested successfully!")
+        return data
+    except Exception as e:
+        logging.error(f"Error ingesting CSV: {str(e)}")
+        
+def ingest_database():
+    try:
+        server = "dmml_customer_churn_setup-sqlserver-1,1433"
+        database = "dmml_assignment"
+        username = "sa"
+        password = "NewPASS1234"
+
+        logging.info("Connecting to Database")
+        connection = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+        logging.info("Connecting to Database Successfull")
+        _ = connection.cursor()
+        
+        query = "SELECT * FROM [dmml_assignment].[dbo].[customers]"
+        logging.info("Reading data from database.")
+        data = pd.read_sql(query, connection)
+        
+        connection.close()
+        p = Path('/opt/airflow/data/raw')
+        p.mkdir(parents = True, exist_ok = True)
+        data.to_csv("/opt/airflow/data/raw/database_data.csv", index=False)
+        logging.info("Data from Database ingested successfully!")
+        return data
+    except Exception as e:
+        logging.error(f"Error ingesting database: {str(e)}")
+        
+filename = 'customer_data.csv';
+csv_data = ingest_csv(filename)
+database_data = ingest_database()
