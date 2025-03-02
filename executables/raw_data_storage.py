a00ca677498f87b8c18076fdaa0e811fb27dcac8
anniemathew-trial <annie.mthw21@gmail.com>
2025-03-02 14:25:26 +0530

    Update raw_data_storage.py
    Signed-off-by: anniemathew-trial <annie.mthw21@gmail.com>

--- executables/raw_data_storage.py
+++ executables/raw_data_storage.py
@@ -6,7 +6,7 @@ import json
 with open("/opt/airflow/executables/settings.json", "r") as file:
         settings = json.load(file)
 #create log file if it does not exist
-ingestion_log_file = f"{settings['logging_base_path']}\\logs\\data_ingestion.log"
+ingestion_log_file = f"{settings['logging_base_path']}/logs/raw_data_storage.log"
 logging.root.handlers = []
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO , filename=ingestion_log_file)
 
